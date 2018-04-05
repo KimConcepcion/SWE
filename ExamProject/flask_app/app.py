c86ffa9f93e80cd65712646ee8550262798c251a
Kim Nielsen <kimnielsen33333@gmail.com>
2018-04-05 20:15:20 +0200

    flask app

---
+++ ExamProject/flask_app/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Flask Dockerized'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='192.168.99.100')
